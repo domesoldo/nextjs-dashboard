2577fd7a61b37cf3a8235d0d7eda00a2a461b5a6
Dom Soldo <domenico.soldo@au.pwc.com>
2023-11-07 15:14:39 +1100

    authentication

---
+++ middleware.ts
@@ -0,0 +1,11 @@
+// NextJS Middleware is called before a request is completed
+
+import NextAuth from "next-auth"
+import { authConfig } from "./auth.config"
+
+export default NextAuth(authConfig).auth
+
+export const config = {
+  // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
+  matcher: ["/((?!api|_next/static|_next/image|.png).*)"],
+}
